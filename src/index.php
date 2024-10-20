5295359db8d2028a79208bd2c2efa1d097d773c9
Mykhailo <michail.yashchuck@gmail.com>
2024-10-20 21:56:43 +0300

    poor list

--- src/index.php
+++ src/index.php
@@ -1,10 +1,12 @@
 <?php
 session_start();
 
-if (isset($_SESSION['user_id'])) {
-    header('Location: todo.php');
-    exit();
-}
+// Хуйня получается какая-то
+
+//if (isset($_SESSION['user_id'])) {
+//    header('Location: todo.php');
+//    exit();
+//}
 ?>
 
 <!DOCTYPE html>
@@ -13,15 +15,15 @@ if (isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To-Do List - Home</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
 <div class="container">
     <h1>Welcome to Your To-Do List App</h1>
     <nav>
-        <a href="src/todo.php">Your Tasks</a>
-        <a href="src/profile.php">Profile</a>
-        <a href="src/logout.php">Logout</a>
+        <a href="todo.php">Your Tasks</a>
+        <a href="profile.php">Profile</a>
+        <a href="logout.php">Logout</a>
     </nav>
 </div>
 </body>
